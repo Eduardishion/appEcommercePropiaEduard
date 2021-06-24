2e3ffe8aee2493f6b8bfbd2c9afe030d8e0af8fc
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-06-24 08:19:45 -0500

    creador de enrutador de vistas

---
+++ routes/rutasVistas.js
@@ -0,0 +1,18 @@
+//objeto express
+const express = require('express');
+//enrutador
+const router = express.Router();
+//controlador de productos 
+const productosController = require('../controllers/vistasController');
+
+//enrutamiento para rutas de productos
+//vista principal
+router.get('/', productosController.vistaIndex);
+//vista index
+router.get('/Index', productosController.vistaIndex);
+//vista detalle producto
+router.get('/DetalleProducto', productosController.vistaDetalleProducto);
+//vista carrito de compra
+router.get('/CarritoCompra', productosController.vistaCarritoCompra);
+
+module.exports = router;
