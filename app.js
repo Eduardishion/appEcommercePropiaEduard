8af2660b841e20bba826d3ccfbe41c105ff80e6e
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-06-24 08:20:40 -0500

    modificacion de entry point para poder usar EJS, rutas y controladores

--- app.js
+++ app.js
@@ -1,35 +1,46 @@
 	//librerias nativas necesarias
 	const express = require('express'); 
-	const path = require('path');
-	
+	//rutas de operaciones CRUD de productos
+	const rutasVistas = require('./routes/rutasVistas');
+	//componente para poder hacer peticiones put y delete
+	const methodOverride = require('method-override');
+
+
+	//------------objeto servidor------------------
 	//objeto servidor 
 	const app = express();
+		//------------objeto servidor------------------
 	
-	//determinar la carpeta de archivos staticos
-	const publicPath = path.resolve(__dirname,'./public');
-	app.use(express.static(publicPath));
-	// 
-	
-	
-	//enrutamiento de endpoints y midellwares
-	app.get('/', (req, res) => {
-	res.sendFile(path.resolve(__dirname,'./views/index.html'));
-	});
-	app.get('/index', (req, res) => {
-	res.sendFile(path.resolve(__dirname,'./views/index.html'));
-	});
-	app.get('/DetalleProducto', (req, res) => {
-	res.sendFile(path.resolve(__dirname,'./views/DetalleProducto.html'));
-	});
-	app.get('/CarritoCompra', (req, res) => {
-	res.sendFile(path.resolve(__dirname,'./views/CarritoCompra.html'));
-	});
+	//------------------midwares------------------
 
-//configuracion de puerto
+	//carpeta publica de archivos estaticos 
+	app.use(express.static('./public'));
+	
+	// cofiguracion para usar  view engine EJS
+	app.set('view engine','ejs');
+
+	//para poder capturar datos de formularios POST
+	app.use(express.urlencoded({extended:false}));
+	app.use(express.json());
+
+	// configurar los metodos delete y put con method-override 
+	app.use(methodOverride('_method'));
+
+	//------------------midwares------------------
+	
+
+	//------------------rutas------------------
+	// rutas de productos 
+	app.use('/', rutasVistas);
+	//------------------rutas------------------	
+
+
+	//------------------entry point------------------	
+	//configuracion de puerto
 	app.set('port', process.env.PORT || 3000 );
 	
 	//configuracion de ruta de acceso al servidor 
 	app.listen(app.get('port'),()=>{
 	console.log(`El servidor se ha inicialiado..., puedes acceder a al mediante http://localhost:${app.get('port')} , para detener preciona Ctrl + C`);
 	});
-	
+	//------------------entry point------------------	
