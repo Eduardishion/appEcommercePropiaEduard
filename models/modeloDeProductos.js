b22f08a9fcdbabc0f86376f9cb0d21844e70ae50
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-07-06 15:43:31 -0500

    se modifico la carga de imagen tanto para post como para put

--- models/modeloDeProductos.js
+++ models/modeloDeProductos.js
@@ -20,7 +20,7 @@ let modeloDeProductos = {
         fs.writeFileSync(path.resolve(__dirname,'productos.json'),cadenaJsonE); 
         
     },
-    estructurarObjeto: function (req) {
+    estructurarObjetoPOST: function (req) {
         
        let fecha = new Date();
 
@@ -44,6 +44,62 @@ let modeloDeProductos = {
         // console.log(req.body.image);
         // console.log(req.body.userWhoRegistered);
         
+        // ---------validacion de imagen---------------
+
+          //validacion de llegada de archivo imagen o no 
+            //req.file para ver las propiedades
+
+            // let imageFile = req.file;
+            // if(imageFile !== underfined){
+
+            // }else{
+            //     console.log("no se cargo la imagen adecuadamente...");
+            //      por i no llegus la imagen podemos volver a renderiar el formulario
+            // }    
+
+            //cuando no se cargue la imagen podemos enviar un por default para que se
+            //cargue aun asi cuando no se cargo la imagen 
+
+            //let nombreImagen = req.file !== underfined ? req.files[0].filename : null ;
+
+            // let nombreImagen = '';
+            
+            // if(req.files){ // si existe 
+            //     console.log(req.files[0].filename);
+            //     console.log(req.files);
+            //     console.log("si se cargo imagen ");
+            //     nombreImagen = req.files[0].filename;
+            // }else{
+            //     console.log("no se cargo imagen ");
+            //     nombreImagen = null;
+            // }
+
+
+            //validacion de imagen si exite nombra la imagen, si no coloca el campo en null
+            // nombreImagen = req.file ? req.files[0].filename : null;
+
+            
+            // console.log(req.files[0].filename); // para usar con any
+            // console.log(req.file.filename); //para usar con single
+            // console.log(req.file.image);
+
+            //como cargar imagen y actuliarla
+            //https://www.it-swarm-es.com/es/node.js/cargue-la-imagen-y-luego-actualice-con-multer-y-express.js/805521310/
+            //para validar que exite una imagen desde el formulario
+            let nombreImagen = '';
+            //if(req.file !== underfined){
+            //si exite el archivo 
+            if(req.file){
+                console.log("--------->"+req.file.filename); //para usar con single
+                nombreImagen = req.file.filename;
+            }else{
+            //si no exite el archivo 
+                nombreImagen = null;
+                // console.log("no se cargo la imagen adecuadamente... se pondra una por default");
+            }    
+
+          
+          // ---------validacion de imagen---------------
 
         
         //creacion de objeto temporal que almacena los datos entrantes de la vista crear producto
@@ -56,7 +112,9 @@ let modeloDeProductos = {
             discount: parseInt(((req.body.price-(req.body.discountRate/100)*req.body.price))), //precio a pagar menos el descuento
             stock: parseInt(req.body.stock),
             description: req.body.description,
-            image: req.files[0].filename,//obtencion de nombre con que se guardo la imagen desde multer, se puede usar tambien req.file.filename;
+            //image: req.files[0].filename,//obtencion de nombre con que se guardo la imagen desde multer, se puede usar tambien req.file.filename con  any(); 
+            //image: req.file.filename,//obtencion de nombre con que se guardo la imagen desde multer, se puede usar tambien req.file.filename con single();
+            image: nombreImagen,//obtencion de nombre con que se guardo la imagen desde multer, se puede usar tambien req.file.filename con single();
             features: req.body.features,
             //extras
             registrationDate: 'dd/mm/yy'.replace(/dd|mm|yy|yyy/gi, matched => map[matched]),
@@ -66,6 +124,118 @@ let modeloDeProductos = {
 
 
         return productoTmp;
+    },
+    estructurarObjetoPUT: function (req, nombreDeimagenNoModificada) {
+        
+        let fecha = new Date();
+ 
+        //para obtener la fecha con formato 
+        const map = {
+             dd: fecha.getDate(),
+             mm:  fecha.getMonth()+1 <= 9 ? '0'+(fecha.getMonth()+1) : (fecha.getMonth()+1),     //podriamos arreglar que agregue un cero en el mes
+             yy: fecha.getFullYear().toString().slice(-2),
+             yyyy: fecha.getFullYear()
+        }
+ 
+         // muestreo de datod principales 
+         // console.log(req.body.id);
+         // console.log(req.body.name);
+         // console.log(req.body.category);
+         // console.log(req.body.price);
+         // console.log(req.body.discountRate);
+         // console.log(req.body.stock);
+         // console.log(req.body.features);
+         // console.log(req.body.description);
+         // console.log(req.body.image);
+         // console.log(req.body.userWhoRegistered);
+         
+         // ---------validacion de imagen---------------
+ 
+           //validacion de llegada de archivo imagen o no 
+             //req.file para ver las propiedades
+ 
+             // let imageFile = req.file;
+             // if(imageFile !== underfined){
+ 
+             // }else{
+             //     console.log("no se cargo la imagen adecuadamente...");
+             //      por i no llegus la imagen podemos volver a renderiar el formulario
+             // }    
+ 
+             //cuando no se cargue la imagen podemos enviar un por default para que se
+             //cargue aun asi cuando no se cargo la imagen 
+ 
+             //let nombreImagen = req.file !== underfined ? req.files[0].filename : null ;
+ 
+             // let nombreImagen = '';
+             
+             // if(req.files){ // si existe 
+             //     console.log(req.files[0].filename);
+             //     console.log(req.files);
+             //     console.log("si se cargo imagen ");
+             //     nombreImagen = req.files[0].filename;
+             // }else{
+             //     console.log("no se cargo imagen ");
+             //     nombreImagen = null;
+             // }
+ 
+ 
+             //validacion de imagen si exite nombra la imagen, si no coloca el campo en null
+             // nombreImagen = req.file ? req.files[0].filename : null;
+ 
+             
+             // console.log(req.files[0].filename); // para usar con any
+             // console.log(req.file.filename); //para usar con single
+             // console.log(req.file.image);
+ 
+             //como cargar imagen y actuliarla
+             //https://www.it-swarm-es.com/es/node.js/cargue-la-imagen-y-luego-actualice-con-multer-y-express.js/805521310/
+             //https://www.it-swarm-es.com/es/node.js/cargue-la-imagen-y-luego-actualice-con-multer-y-express.js/805521310/
+             //para validar que exite una imagen desde el formulario
+             let nombreImagen = '';
+             //if(req.file !== underfined){
+             //si exite el archivo 
+             if(req.file){
+                //pero si viene el archivo se debe actulsar
+                nombreImagen = req.file.filename;
+
+                //si es el caso que que si se valla a cargar otra imagen 
+                //hay que eliminar anterior 
+
+                //eliminacion de imagen previamente cargada por otro y no exista despus de ser actualisada con nueva imagen
+                this.eliminarArchivoImagen(nombreDeimagenNoModificada);
+             }else{
+               //si no exite el archivo, se usa la misma que ya se tenia cargada 
+                nombreImagen = nombreDeimagenNoModificada;
+                // console.log("no se cargo la imagen adecuadamente... se pondra una por default");
+             }    
+ 
+           
+           // ---------validacion de imagen---------------
+ 
+         
+         //creacion de objeto temporal que almacena los datos entrantes de la vista crear producto
+         let productoTmp = {
+             id: parseInt(req.body.id),
+             name: req.body.name,
+             category: req.body.category,
+             price: parseInt(req.body.price),
+             discountRate: parseInt(req.body.discountRate),
+             discount: parseInt(((req.body.price-(req.body.discountRate/100)*req.body.price))), //precio a pagar menos el descuento
+             stock: parseInt(req.body.stock),
+             description: req.body.description,
+             //image: req.files[0].filename,//obtencion de nombre con que se guardo la imagen desde multer, se puede usar tambien req.file.filename con  any(); 
+             //image: req.file.filename,//obtencion de nombre con que se guardo la imagen desde multer, se puede usar tambien req.file.filename con single();
+             image: nombreImagen,//obtencion de nombre con que se guardo la imagen desde multer, se puede usar tambien req.file.filename con single();
+             features: req.body.features,
+             //extras
+             registrationDate: 'dd/mm/yy'.replace(/dd|mm|yy|yyy/gi, matched => map[matched]),
+             checkInTime: fecha.getHours()+":"+fecha.getMinutes()+" "+(fecha.getHours() >= 12 ? 'PM' : 'AM'),
+             userWhoRegistered: req.body.userWhoRegistered,
+         }; 
+ 
+ 
+         return productoTmp;
     },
     buscarProducto: function (listaProductos, req) {
         let productoEncontrado = listaProductos.find( (producto) => {
@@ -87,7 +257,7 @@ let modeloDeProductos = {
 
          //console.log(rutaImagen);
          fs2.unlink(rutaImagen).then( ()=>{
-            console.log('Se elimino archivo de imagen...');
+            console.log('Se elimino archivo de imagen... al actuliar datos');
          }).catch( err =>{
             console.error('No se pudo eliminar el archivo no exite',err);
          });
