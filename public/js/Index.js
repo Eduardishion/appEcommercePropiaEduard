1d670476463ff7c9de2497098de316a07ce13cd7
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-09-08 12:21:13 -0500

    aplicacion de animaciones al pasar mouse

---
+++ public/js/Index.js
@@ -0,0 +1,24 @@
+// https://www.w3schools.com/jsref/prop_style_transform.asp
+// https://developer.mozilla.org/es/docs/Web/CSS/transform-function/scale()
+window.addEventListener('load', function () {
+    console.log("gola mundo desde index ");
+
+    const productos = document.querySelectorAll('.articulo.item');
+
+    for (let i = 0; i < productos .length; i++) {
+        
+        productos[i].addEventListener('mouseover', function () {
+             console.log('has pasado por encima de producto');
+             productos[i].style.transform = "scale(1.1)";
+             
+        });
+
+        productos[i].addEventListener('mouseout', function () {
+            console.log('has pasado por encima de producto');
+            productos[i].style.transform = "scale(1)";
+            
+       });
+    }
+
+
+});
