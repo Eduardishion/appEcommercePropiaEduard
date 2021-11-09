90e5b7883e4b7b9bb64ace357d43c59dd44b83f2
Eduardo Izquierdo Rojas <softwareengineer.eduardo.uapt@gmail.com>
2021-11-08 18:51:38 -0600

    correccion de seeder de imagen

--- database/seeders/20210825195124-image.js
+++ database/seeders/20210825195124-image.js
@@ -14,34 +14,34 @@ module.exports = {
 
     await queryInterface.bulkInsert('images', [
     {
-        image: "imagesProducto-1626710613282.webp",
+        name: "imagesProducto-1626710613282.webp",
         product_id: 1,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
     },
     {
-        image: "imagesProducto-1626710613283.webp",
+        name: "imagesProducto-1626710613283.webp",
         product_id: 1,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
    
     },
     {
-        image:"imagesProducto-1626710613284.webp",
+        name:"imagesProducto-1626710613284.webp",
         product_id: 1,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
         
     },
     {
-        image: "imagesProducto-1626710613285.webp",
+        name: "imagesProducto-1626710613285.webp",
         product_id: 1,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
         
     },
     {
-        image: "imagesProducto-1626710613286.webp",
+        name: "imagesProducto-1626710613286.webp",
         product_id: 1,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
@@ -52,34 +52,34 @@ module.exports = {
 
 
     {
-        image: "imagesProducto-1626710613282.webp",
+        name: "imagesProducto-1626710613282.webp",
         product_id: 2,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
     },
     {
-        image: "imagesProducto-1626710613283.webp",
+        name: "imagesProducto-1626710613283.webp",
         product_id: 2,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
    
     },
     {
-        image:"imagesProducto-1626710613284.webp",
+        name:"imagesProducto-1626710613284.webp",
         product_id: 2,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
         
     },
     {
-        image: "imagesProducto-1626710613285.webp",
+        name: "imagesProducto-1626710613285.webp",
         product_id: 2,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
         
     },
     {
-        image: "imagesProducto-1626710613286.webp",
+        name: "imagesProducto-1626710613286.webp",
         product_id: 2,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
@@ -88,34 +88,34 @@ module.exports = {
 
 
     {
-        image: "imagesProducto-1626710613282.webp",
+        name: "imagesProducto-1626710613282.webp",
         product_id: 3,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
     },
     {
-        image: "imagesProducto-1626710613283.webp",
+        name: "imagesProducto-1626710613283.webp",
         product_id: 3,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
    
     },
     {
-        image:"imagesProducto-1626710613284.webp",
+        name:"imagesProducto-1626710613284.webp",
         product_id: 3,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
         
     },
     {
-        image: "imagesProducto-1626710613285.webp",
+        name: "imagesProducto-1626710613285.webp",
         product_id: 3,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
         
     },
     {
-        image: "imagesProducto-1626710613286.webp",
+        name: "imagesProducto-1626710613286.webp",
         product_id: 3,
         createdAt: "19/07/21",
         updatedAt: "19/07/21"
