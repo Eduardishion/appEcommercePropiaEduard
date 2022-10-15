2d6b9841d03586e7b41410c11f8f3857df69e3f4
Eduardo Izquierdo Rojas <eduardo.izquierdo@lottuseducation.com>
2022-10-14 19:32:41 -0500

    cambios minimos

--- database/config/config.js
+++ database/config/config.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "12345",
-    "database": "DB_ecommerce_v2",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "port": "3306"
+    "username":  process.env.USERNAME_DB,
+    "password":  process.env.PASSWORD_DB,
+    "database":  process.env.DATABASE_DB,
+    "host": process.env.HOST_DB,
+    "dialect": process.env.DIALECT_DB,
+    "port": process.env.PORT_DB
   },
   "test": {
     "username": "root",
